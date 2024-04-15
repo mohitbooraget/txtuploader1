1a9e311670c5cc6ca67e42f481e6645d9e206924
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-04-15 22:09:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6756823663:AAFVW3NDF0EB2UsTarqMxVqi0g5_ZjXXs3o")
-    API_ID = int(os.environ["API_ID", 27060147]
-    API_HASH = os.environ["API_HASH", "70f93c98c61a40f69c0f830bd4fece7c"]
-    AUTH_USERS = "6693536228,5684410709,1318247204,1511103739 """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7079450147:AAECUtUv7O97b1IhYyYULLY4FY0X6U7UOQ0")
+    API_ID = int(os.environ["API_ID", 24147139]
+    API_HASH = os.environ["API_HASH", "17f525f59de52108805a65f5ffd909e4"]
+    AUTH_USERS = "5186250641,6594402123,5331045824,5935537622,5724356290,6436809802 """
 
